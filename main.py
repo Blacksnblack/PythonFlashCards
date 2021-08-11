2fa1ce3f2f2e9328369d07a213be81f162298f04
Terry Black <blacksnblack@gmail.com>
2021-08-10 23:33:45 -0600

    add main python app
    uses tkinter for visuals

---
+++ main.py
@@ -0,0 +1,140 @@
+import tkinter as tk
+
+fullCard = False
+cardIndex = 0
+fbIndex = 0
+cards = []
+card = None
+filename = "../vocab/verbs.txt" #  "../vocab/test.txt"
+horizontalRule = "-" * 40
+
+def formatText(text):
+    # Word Wrap
+    temp = text.split("\n")
+    pos = 0
+    for i, item in enumerate(temp):
+        if len(item) > 20 and item != horizontalRule:  # ignore horizontal rule
+            if " " in text[pos+20:]:
+                while not text[pos+20] == " ":
+                    pos += 1
+            elif " " in text[:pos+20]:
+                while not text[pos+20] == " ":
+                    pos -= 1
+            text = text[:pos+20] + "\n" + text[pos+20:]
+        pos += len(item) + 1
+
+    # dynamic vertical center
+    num = text.count("\n")
+    if num <= 2:
+        return "\n" * 3 + text
+    elif num <= 4:
+        return "\n" * 2 + text
+    else:
+        return "\n" + text
+
+
+def doFullCard():
+    fullText = cards[cardIndex][0] + "\n" + horizontalRule + "\n" + cards[cardIndex][1]
+    fullText = formatText(fullText)
+    card.config(text=fullText)
+
+
+def showFullCard():
+    global card, cards, cardIndex, fullCard
+    fullCard = not fullCard
+    if fullCard:
+        doFullCard()
+    else:
+        updateCard()
+
+
+def updateCard():
+    global card, cards, cardIndex, fbIndex
+    card.config(text=formatText(cards[cardIndex][fbIndex]))
+
+
+def nextCard():
+    global cardIndex, cards, fullCard
+    if cardIndex < len(cards) - 1:
+        cardIndex += 1
+        if fullCard:
+            doFullCard()
+        else:
+            updateCard()
+
+
+def previousCard():
+    global cardIndex, cards, fullCard
+    if cardIndex > 0:
+        cardIndex -= 1
+    if fullCard:
+        doFullCard()
+    else:
+        updateCard()
+
+
+def flipCard():
+    global fbIndex, fullCard
+    if fullCard:
+        return
+    fbIndex += 1
+    if fbIndex > 1:
+        fbIndex = 0
+    updateCard()
+
+
+def generateCards():
+    with open(filename, "r", encoding="utf8") as f:
+        data = f.read().split("\n\n")
+        # print(data)
+        for item in data:
+            cardData = item.split("~")
+            if len(cardData) > 1:
+                cards.append(cardData)
+
+
+WIDTH = 400
+HEIGHT = 400
+
+root = tk.Tk()
+root.geometry(f"{WIDTH}x{HEIGHT}")
+root.resizable(False, False)
+
+mainFrame = tk.Frame(root)
+mainFrame.grid()
+
+# Top
+topFrame = tk.Frame(mainFrame, width=WIDTH, height=50)
+topFrame.grid(row=0, column=0)
+topFrame.columnconfigure(0, weight=10)
+
+optionButton = tk.Button(topFrame, text="Show Both Sides", command=showFullCard)
+optionButton.grid()
+
+# Center
+entryFrame = tk.Frame(mainFrame, width=WIDTH, height=HEIGHT - 100)
+entryFrame.grid(row=1, column=0)
+entryFrame.columnconfigure(0, weight=10)
+entryFrame.grid_propagate(False)
+
+card = tk.Label(entryFrame, text="私は猫です。\n あなたも猫ですか？", font=("Arial", 20))  # , width=42, height=10)
+card.grid(row=0, column=0, columnspan=3)
+
+generateCards()
+updateCard()
+
+# Bottom
+bottomFrame = tk.Frame(mainFrame, width=WIDTH, height=50)
+bottomFrame.grid(row=2, column=0)
+bottomFrame.columnconfigure(0, weight=10)
+
+back_button = tk.Button(bottomFrame, text="<--", command=previousCard, padx=15)
+flip_button = tk.Button(bottomFrame, text="Flip", command=flipCard, padx=15)
+forward_button = tk.Button(bottomFrame, text="-->", command=nextCard, padx=15)
+
+back_button.grid(row=1, column=0)
+flip_button.grid(row=1, column=1)
+forward_button.grid(row=1, column=2)
+
+# mainloop
+root.mainloop()
